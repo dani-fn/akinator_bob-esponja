26631b06321dd9519adeb5a0a28ded28536f4270
Daniel Ferreira Natal <71156976+dani-fn@users.noreply.github.com>
2023-10-18 20:08:15 -0300

    Inicio do desenvolvimento

---
+++ src/main/java/com/mycompany/akinator_bobesponja/Akinator_bobesponja.java
@@ -0,0 +1,12 @@
+package com.mycompany.akinator_bobesponja;
+
+import java.util.Scanner;
+
+public class Akinator_bobesponja {
+
+    public static void main(String[] args) {
+
+        JogoPerguntas jogo = new JogoPerguntas();
+        jogo.iniciarJogo();
+    }
+}
