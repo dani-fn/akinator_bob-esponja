cdd495f1b62e8ab4fb96928c4ea0dd0bfbdbcc19
Daniel Ferreira Natal <71156976+dani-fn@users.noreply.github.com>
2023-10-18 21:23:46 -0300

    Add Classe Personagens

---
+++ src/main/java/com/mycompany/akinator_bobesponja/Personagens.java
@@ -0,0 +1,22 @@
+package com.mycompany.akinator_bobesponja;
+
+public class Personagens {
+    public String nome;
+    public String especie;
+    //public String endereco;
+    public String profissao;
+    public String localTrabalho;
+    public char sexo;
+    //public boolean familiar;
+    //public boolean propriedade;
+
+    public Personagens(String nome, String especie, String endereco, String profissao, String localTrabalho, char sexo) {
+        this.nome = nome;
+        this.especie = especie;
+        this.profissao = profissao;
+        this.localTrabalho = localTrabalho;
+        this.sexo = sexo;
+    }
+    
+    
+}
